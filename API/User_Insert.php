9ee740f6cae3f6fe52ed25cae9f662ab16a94ad6
BardiaMadadi <bardiamadadi86@gmail.com>
2021-07-14 13:40:23 +0430

    Add response functions

---
+++ API/User_Insert.php
@@ -0,0 +1,58 @@
+<?php
+require '../class/user.php';
+
+header('Content-Type: application/json');
+
+
+if (
+    isset($_POST['userName']) &&
+    isset($_POST['phoneNumber']) &&
+    isset($_POST['pwd']) &&
+    isset($_POST['birthday']) &&
+    isset($_POST['accountLevel']) &&
+    isset($_POST['created']) &&
+    isset($_POST['end'])
+) {
+
+    $userName = $_POST['userName'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $pwd = $_POST['pwd'];
+    $birthday = $_POST['birthday'];
+    $accountLevel = $_POST['accountLevel'];
+    $created = $_POST['created'];
+    $end = $_POST['end'];
+
+
+    $user = new user($userName, $phoneNumber, $pwd, $birthday, $accountLevel, $created, $end);
+    $user->insertUser();
+
+
+    response(200, "User Created", array(
+        "userName" => $userName,
+        "phoneNumber" => $phoneNumber,
+        "birthday" => $birthday,
+        "accountLevel" => $accountLevel,
+        "created" => $created,
+        "end" => $end,
+    ));
+} else {
+    var_dump($_POST);
+    response(400, "You Shold send all things", null);
+}
+
+
+function response($code, $message, $data)
+{
+    $response['code'] = $code;
+    $response['message'] = $message;
+    if ($code == 200) {
+        $response['userName'] = $data['userName'];
+        $response['phoneNumber'] = $data['phoneNumber'];
+        $response['birthday'] = $data['birthday'];
+        $response['accountLevel'] = $data['accountLevel'];
+        $response['created'] = $data['created'];
+        $response['end'] = $data['end'];
+    }
+    echo json_encode($response, true);
+}
+
