b5355616d4998eff7833c55b9b345f9d9a2625ba
BardiaMadadi <bardiamadadi86@gmail.com>
2021-08-16 23:50:39 +0430

    Add User Update API

---
+++ API/User_Upgrade.php
@@ -0,0 +1,21 @@
+<?php
+
+
+if(isset($_POST['Level'],$_POST["uId"])){
+    if($_POST['Level'] == "bronze" || $_POST['Level'] == "steel" || $_POST['Level'] == "gold" || $_POST['Level'] == "diamond"){
+
+        require_once "../class/user.php";
+        $user = new User;
+        $user->upgrade_user($_POST['Level'],$_POST["uId"]);
+        
+    }else{
+        response_post_question(454,"SET VALID LEVEL",null);
+
+    }
+    
+}else{
+    response_post_question(454,"PLS SET SOME VARS",null);
+}
+
+
+
